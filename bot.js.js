a0ca20ebdd72c11d9cd1ad3b08293f658b551be8
mohmmedmando <45184106+mohmmedmando@users.noreply.github.com>
2019-02-22 15:30:28 +0200

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("play!",{type: 'Playing'})
+   client.user.setActivity("!play",{type: 'Playing'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
